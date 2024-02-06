f523c1a963e63a881a35bc992bc59c619e78407f
Matty47ghigo <121062486+Matty47ghigo@users.noreply.github.com>
2024-02-06 07:33:52 +0000

    Last commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     hmr: true, // Change this line to false disable auto-refreshing.
-  }
-})
+  },
+});
